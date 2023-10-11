60eaca53dc807b0db95d0859d41cbe21644c9167
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-10-11 09:54:03 -0300

    Adding id's and 'details' pages

--- Beakem2.0/index.js
+++ Beakem2.0/index.js
@@ -1,52 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-app.use(express.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const albums = [
-    {
-        album: 'Rumours',
-        rate: 10
-    },
-    {
-        album: 'The Marhsall Mathers LP',
-        rate: 10
-    },
-    {
-        album: 'Pet Sounds',
-        rate: 9.7
-    },
-    {
-        album: 'The Blueprint',
-        rate: 9.6
-    },
-    {
-        album: 'DAMN',
-        rate: 9.4
-    },
-    {
-        album: 'A Day At The Races',
-        rate: 9.2
-    },
-]
+  {
+    id: uuidv4(),
+    album: "Rumours",
+    rate: 10,
+  },
+  {
+    id: uuidv4(),
+    album: "The Marhsall Mathers LP",
+    rate: 10,
+  },
+  {
+    id: uuidv4(),
+    album: "Pet Sounds",
+    rate: 9.7,
+  },
+  {
+    id: uuidv4(),
+    album: "The Blueprint",
+    rate: 9.6,
+  },
+  {
+    id: uuidv4(),
+    album: "DAMN",
+    rate: 9.4,
+  },
+  {
+    id: uuidv4(),
+    album: "A Day At The Races",
+    rate: 9.2,
+  },
+];
 
-app.get('/albums', (req, res) => {
-    res.render('albums/index', {albums})
-})
+app.get("/albums", (req, res) => {
+  res.render("albums/index", { albums });
+});
 
-app.get('/albums/new', (req, res)=>{
-    res.render('albums/new')
-})
+app.get("/albums/new", (req, res) => {
+  res.render("albums/new");
+});
 
-app.post('/albums', (req, res)=>{
-    const {album, rate} = req.body;
-    albums.push({album, rate})
-    res.redirect('/albums')
-})
+app.post("/albums", (req, res) => {
+  const { album, rate } = req.body;
+  albums.push({ album, rate, id: uuidv4() });
+  res.redirect("/albums");
+});
 
+app.get("/albums/:id", (req, res) => {
+  const { id } = req.params;
+  const album = albums.find((a) => a.id === id);
+  res.render("albums/show", { album });
+});
 
 // app.post('/album', (req, res)=>{
 //     const {album, rating} = req.body;
@@ -59,6 +71,6 @@ app.post('/albums', (req, res)=>{
 // }
 // })
 
-app.listen(2000, ()=>{
-    console.log('lsitening on port 2000')
-})
+app.listen(2000, () => {
+  console.log("lsitening on port 2000");
+});
