595febeea02c0b21f2a9f052dfe41193fff6ec96
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-11-29 11:51:52 -0300

    Adding ejs-mate and bootstrap

--- Beakem3.0/index.js
+++ Beakem3.0/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 
 const Album = require("./models/album");
@@ -16,6 +17,7 @@ mongoose
     console.log(err);
   });
 
+app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
